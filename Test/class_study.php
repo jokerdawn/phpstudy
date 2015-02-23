2e0ad1c3a8906d008b98bd3fc5dad89ee9df3876
jokerdawn <jdse88@gmail.com>
2015-02-23 15:51:52 +0800

    Signed-off-by: jokerdawn <jdse88@gmail.com>

--- Test/class_study.php
+++ Test/class_study.php
@@ -1,37 +1,27 @@
 <?php
-	class cal {
-		public $num = array();
-		public $para = array();
-		public $array_num;
+	
+	class fontcolor {
+		public $color;
 
-		function extract_parameter() {
-			$str = $this->formula;
-			echo $str;
-			//preg_match('/^[?<=<(\w+)>]$/', $this->formula,$para); 匹配算法符号
-			//preg_match('/^(?<=({}))\w{0,1}(?=(}))$/', $this->formula,$matches);
-			if(preg_match('/^\S*{x}\S*$/', $str)){
-				echo 'ture';
-			}else {
-				echo 'false';
-			}
+		function CHGcolor (){
+			echo "<font color = '$this->color'>Color</font>";
+		} 
+	}
+?>
 
-			var_dump($matches);
-			$array_num = count($matches);
-		}
+<HTML>
+	<form method = 'GET'>
+		<input type = 'text' name = 'color'/><input type = 'submit' name = 'submit'/>
+	</form>
+</HTML>
 
-//		function calc(){
-
-
-//		}
-
+<?php 
+	if(!isset($_GET['submit'])||empty($_GET['submit'])){
+		echo 'please input color';
+		exit();
 	}
 
-	$a = new cal();
-	$a->formula = '{x}+{y}' ;//formula but not equation.
-	//echo $a->formula;
-	//var_dump($a->formula);
-	$a->extract_parameter();
-	//var_dump($a->para);
-	//var_dump($a->array_num);
-	echo $a->array_num;
+	$a = new fontcolor();
+	$a->color = $_GET['color'];
+	$a->CHGcolor();
 ?>
