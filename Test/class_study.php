21134686cc36e371fe194577a7293124a19f275f
jokerdawn <jdse88@gmail.com>
2015-02-23 21:52:24 -0800

    类学习：父类与继承 Signed-off-by: jokerdawn <jdse88@gmail.com>

--- Test/class_study.php
+++ Test/class_study.php
@@ -1,27 +1,53 @@
 <?php
 	
 	class fontcolor {
-		public $color;
+		public $color = array('R'=>0,'G'=>0,'B'=>0);
 
 		function CHGcolor (){
-			echo "<font color = '$this->color'>Color</font>";
+			$B = $this->color['B'];
+			$R = $this->color['R'];
+			$G = $this->color['G'];
+			echo "<font style = 'color:rgb($R,$G,$B)'>Color</font>";
+		} 
+	}
+
+	class transparent extends fontcolor {
+		public $Alpha = 1;
+
+		function CHGcolor(){
+			$B = $this->color['B'];
+			$R = $this->color['R'];
+			$G = $this->color['G'];
+			echo "<font style = 'color:rgba($R,$G,$B,$this->Alpha)'>Color</font>";
 		} 
 	}
 ?>
 
 <HTML>
 	<form method = 'GET'>
-		<input type = 'text' name = 'color'/><input type = 'submit' name = 'submit'/>
+		R <input type = 'text' name = 'R' style="width:30px" value = '0'/>  G <input type = 'text' name = 'G' style="width:30px" value = '0'/> B <input type = 'text' name = 'B' style="width:30px" value = '0'/></br>
+		<input type = 'checkbox' name = 'tp' value="half-color"></br>
+		<input type = 'submit' name = 'submit'/>
+
 	</form>
 </HTML>
-
+ 
 <?php 
 	if(!isset($_GET['submit'])||empty($_GET['submit'])){
 		echo 'please input color';
 		exit();
 	}
 
-	$a = new fontcolor();
-	$a->color = $_GET['color'];
+	//var_dump($_GET);
+
+	$a = new transparent();
+	$a->color['R'] = $_GET['R'];
+	$a->color['G'] = $_GET['G'];
+	$a->color['B'] = $_GET['B'];
+	if(isset($_GET['tp'])&&($_GET['tp'] == 'half-color')) {
+		$a->Alpha = 0.5;
+	}else{
+		$a->Alpha = 1;
+	}
 	$a->CHGcolor();
 ?>
