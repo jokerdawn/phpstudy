910117bde63a5071d8ee321451981db3a0fd1b45
jokerdawn <jdse88@gmail.com>
2015-03-28 13:26:16 +0800

    字幕转换模块，AJAX使用POST，利用正则表达式全文搜索替换粗时间为时间轴。 可用于Youtube字幕转时间轴。 Signed-off-by: jokerdawn <jdse88@gmail.com>

--- Test/st/trans.php
+++ Test/st/trans.php
@@ -1,28 +1,28 @@
 <?php
-	if(!isset($_GET['content'])||empty($_GET['content'])){
-		echo "alert('content is empty!');window.location.href = 'st.html';";
+	if(!isset($_POST['content'])||empty($_POST['content'])){
+		echo "content is empty!";
+		exit();
+	}
+	if(!isset($_POST['endtime'])||empty($_POST['endtime'])){
+		echo 'Please input endtime.';
 		exit();
 	}
 
-	$content = $_GET['content'];
-	$end_time = '60:00:00,000';
-	
-	//echo $content;
+	$content = $_POST['content'];
+	$end_time = $_POST['endtime'];
 
-	//str format
+	//srt format
 	//1   //No.
 	//hh:mm:ss,XXX -> hh:mm:ss,XXX  //Timeline
 	//Content
 	//1</br>hh:mm:ss -> hh:mm:ss,XXX  //HTML format
 
 	preg_match_all('/\d{1,4}:\d{1,2}/', $content,$matches);
-	//var_dump($matches);
+	if(empty($matches[0])){
+		exit();
+	}
 	$qunti = count($matches[0]);
-	/*//echo $qunti;
 
-	for ($i = 0;$i < $qunti ;$i++){
-		echo $matches[0][$i].'</br>';
-	}*/
 	$patch = array();
 
 	for($i = 0;$i<$qunti;$i++){
@@ -33,19 +33,18 @@
 		}
 	}
 	
-	for($c = 0;$c<$qunti-1;$c++){
+	for($c = 0;$c<$qunti-1;$c++){  //No.1~12, so qunti need`s minus 1
 		$b = $c +1;
-		$str[$c] = ($b).'</br>'.$patch[$c].'->'.$patch[$b];
+		$str[$c] = ($b).'</br>'.$patch[$c].'->'.$patch[$b].'<';
 	}
+	$str[$qunti-1] = $qunti.'</br>'.$patch[$c].'->'.$end_time.'<';
 
-	$str[$qunti-1] = $qunti.'</br>'.$patch[$c].'->'.$end_time;
-
-	var_dump($str);
-
-	for ($i = 0;$i < $qunti ;$i++){
-		echo $str[$i].'</br>';
+	for ($j = 0,$match_patt = array();$j < $qunti-1 ;$j++){
+		$match_patt[$j] = '/\b'.$matches[0][$j].'\b\</';
 	}
-	//preg_replace('/\d{1,4}:\d{1,2}/', replacement, subject)
-	
+	$match_patt[$qunti-1] = '/\b'.$matches[0][$j].'\b\</';
 
+
+	$trans = preg_replace($match_patt, $str, $content);
+	echo $trans;
 ?>
